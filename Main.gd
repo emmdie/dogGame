e8acc5559e22bf36b66c3f8627dc18c60389ef2c
emmdie <emmanuel.diehl@ifsr.de>
2022-08-11 14:37:43 +0200

    added music and licenses

---
+++ Main.gd
@@ -0,0 +1,7 @@
+extends Node
+
+export(PackedScene) var stone_scene
+var score
+
+func _ready():
+	randomize()
---
+++ assets/music/Readme.txt
@@ -0,0 +1,1 @@
+Praerie Oyster by Ben Reber, CC BY 4.0 Licence https://creativecommons.org/licenses/by/4.0/
