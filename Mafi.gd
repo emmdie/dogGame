9546340288610b9abcaa5d6300618e0b572218f9
N D <klokaktus@gmx.de>
2022-08-11 12:03:00 +0200

    added the keybindings for moving up and down and the ability to move

--- Mafi.gd
+++ Mafi.gd
@@ -12,6 +12,23 @@ func _ready():
 	screen_size = get_viewport_rect().size
 
 
+func _process(delta):
+	var velocity = Vector2.ZERO #the players movement vector
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -=1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	
+	position += velocity * delta
+	position.x = clamp(position.y, 0, screen_size.y)
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
