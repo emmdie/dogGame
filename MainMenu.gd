307c7092c743e7cfa437aadb847056a941de635d
emmdie <emmanuel.diehl@ifsr.de>
2022-08-12 19:02:58 +0200

    clicking about opens README.md on Github

---
+++ MainMenu.gd
@@ -0,0 +1,20 @@
+extends MarginContainer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_AboutButton_pressed():
+	OS.shell_open("https://github.com/emmdie/dogGame/blob/main/README.md")
